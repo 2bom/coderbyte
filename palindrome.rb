d583d2866096c880af5326c4abb4c8e616fdedb6
Lee Bo-min <goilria2@gmail.com>
2015-07-31 16:10:35 +0900

    Create palindrome.rb

---
+++ palindrome.rb
@@ -0,0 +1,16 @@
+def Palindrome(str)
+
+  # code goes here
+  a = str.delete(' ')
+  
+  check = 'flase'
+  if a == a.reverse
+    check = 'true'
+  end  
+    return check
+  
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+Palindrome(STDIN.gets)           
